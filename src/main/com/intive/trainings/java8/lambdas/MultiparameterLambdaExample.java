a50b81cf67311961f86cbd432e883adfa8f21ab0
Wawrzkowicz, Rafal <ext-rafal.wawrzkowicz@f-secure.com>
2018-12-16 20:36:47 +0100

    quick rename

--- src/main/com/intive/trainings/java8/lambdas/MultiparameterLambdaExample.java
+++ src/main/com/intive/trainings/java8/lambdas/MultiparameterLambdaExample.java
@@ -8,7 +8,7 @@ public class MultiparameterLambdaExample {
      * You have to define functional interface with method taking as many params as you need.
      */
     @FunctionalInterface
-    interface Function<A, B, C, D, E> {
+    interface MultiParamFunction<A, B, C, D, E> {
         E apply(A a, B b, C c, D d);
     }
 
@@ -16,11 +16,10 @@ public class MultiparameterLambdaExample {
         /*
          * After then you you can define as many different lambdas as you want.
          * */
-
-        Function<Integer, String, Double, Boolean, Integer> multiparamFunction =
+        MultiParamFunction<Integer, String, Double, Boolean, Integer> multiparamFunction =
                 (a, b, c, d) -> 2;
 
-        Function<Integer, Integer, Integer, Integer, Long> f =
+        MultiParamFunction<Integer, Integer, Integer, Integer, Long> f =
                 (a, b, c, d) -> (long) IntStream.of(a, b, c, d).reduce(0, (aa, bb) -> aa * 10 + bb * 100);
 
     }
